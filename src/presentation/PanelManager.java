5a8700003d806730d257bae886fec9159c8c3650
just-juanma <jugoka947@gmail.com>
2023-06-14 20:03:54 -0300

    front

--- src/presentation/PanelManager.java
+++ src/presentation/PanelManager.java
@@ -5,6 +5,15 @@ import javax.swing.JFrame;
 public class PanelManager {
     private JFrame frame;
     // private PromoterRegistrationPanel promoterRegistrationPanel;
-    // private 
+    // private PromoterListPanel promoterListPanel;
+
+    public PanelManager() {
+        initializeManager();
+    }
+
+    public void initializeManager() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 500, 500);
+    }
 
 }
