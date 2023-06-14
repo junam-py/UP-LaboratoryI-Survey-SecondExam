b147b3b86a8e3b1c65b51f59b972f6f71bccdc95
just-juanma <jugoka947@gmail.com>
2023-06-14 19:38:32 -0300

    db doc

--- src/database/DBManager.java
+++ src/database/DBManager.java
@@ -13,8 +13,11 @@ public class DBManager {
     private static final String DB_DRIVER = "org.h2.Driver";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASSWORD = "";
-
-    // connection
+    
+    /** 
+     * @return Connection result
+     * @throws DBManagerException in case of error loading driver or connection to DB
+     */
     public static Connection connect() throws DBManagerException {
         Connection connection = null;
         try {
@@ -33,11 +36,18 @@ public class DBManager {
         return connection;
     }
 
+    /** 
+     * @return String of the absolutePath
+     */
     public static String getBaseDirectory() {
         File curr = new File("h2/database/");
         return curr.getAbsolutePath();
     }
 
+    /** 
+     * @param connection to close
+     * @throws DBManagerException in case of error closing connection
+     */
     public static void closeConnection(Connection connection) throws DBManagerException {
         try {
             connection.close();
@@ -46,7 +56,11 @@ public class DBManager {
         }
     }
 
-    // statement
+    /** 
+     * @param connection to create statement
+     * @return Statement created by connection
+     * @throws DBManagerException in case of error creating statement
+     */
     public static Statement createStatement(Connection connection) throws DBManagerException {
         try {
             return connection.createStatement();
@@ -54,7 +68,13 @@ public class DBManager {
             throw new DBManagerException("Error creating new database statement");
         }
     }
-
+    
+    /** 
+     * @param connection in case of rollback connection 
+     * @param statement to execute 
+     * @param sql string to execute
+     * @throws DBManagerException in case of error executing statement
+     */
     public static void executeStatement(Connection connection, Statement statement, String sql)
             throws DBManagerException {
         try {
@@ -64,7 +84,13 @@ public class DBManager {
             throw new DBManagerException("Error executing statement");
         }
     }
-    
+     
+    /** 
+     * @param connection in case of rollback connection 
+     * @param statement to execute update
+     * @param sql string to execute update
+     * @throws DBManagerException in case of error executing update
+     */
     public static void executeUpdate(Connection connection, Statement statement, String sql) throws DBManagerException {
         try {
             statement.executeUpdate(sql);
@@ -73,8 +99,14 @@ public class DBManager {
             throw new DBManagerException("Error executing update");
         }
     }
-
-    public static ResultSet executeQuery(Connection connection, Statement statement, String sql) throws DBManagerException {
+    
+    /** 
+     * @param statement to execute query
+     * @param sql string to execute query
+     * @return ResultSet of query
+     * @throws DBManagerException in case of error executing query
+     */
+    public static ResultSet executeQuery(Statement statement, String sql) throws DBManagerException {
         ResultSet rs = null;
         try {
             rs = statement.executeQuery(sql);
@@ -84,6 +116,10 @@ public class DBManager {
         }
     }
 
+    /** 
+     * @param connection to rollback
+     * @throws DBManagerException in case of error rolling back
+     */
     public static void rollback(Connection connection) throws DBManagerException {
         try {
             connection.rollback();
@@ -92,6 +128,10 @@ public class DBManager {
         }
     }
 
+    /** 
+     * @param connection to commit
+     * @throws DBManagerException in case of error commiting
+     */
     public static void commit(Connection connection) throws DBManagerException {
         try {
             connection.commit();
@@ -100,6 +140,11 @@ public class DBManager {
             throw new DBManagerException("Error doing commit");
         }
     }
+    
+    /** 
+     * @param sql string to generate statement execution update
+     * @throws DBManagerException in case of error generating statement execution update
+     */
     public static void generateStatementExUpdate(String sql) throws DBManagerException {
         Connection connection = DBManager.connect();
         Statement statement = DBManager.createStatement(connection);
@@ -107,7 +152,11 @@ public class DBManager {
         DBManager.commit(connection);
         DBManager.closeConnection(connection);
     }
-
+    
+    /** 
+     * @param sql string to generate statement execution
+     * @throws DBManagerException in case of error generating statement execution
+     */
     public static void generateStatementEx(String sql) throws DBManagerException {
         Connection connection = DBManager.connect();
         Statement statement = DBManager.createStatement(connection);
@@ -115,11 +164,17 @@ public class DBManager {
         DBManager.commit(connection);
         DBManager.closeConnection(connection);
     }
-
+ 
+    /** 
+     * @param sql string to generate query
+     * @return ResultSet of the query
+     * @throws DBManagerException in case of error generating query
+     */
     public static ResultSet generateQuery(String sql) throws DBManagerException {
         Connection connection = DBManager.connect();
         Statement statement = DBManager.createStatement(connection);
-        ResultSet rs = DBManager.executeQuery(connection, statement, sql);
+        ResultSet rs = DBManager.executeQuery(statement, sql);
         return rs;  
     }
+    
 }
--- src/database/tablemanager/PromoterTableManager.java
+++ src/database/tablemanager/PromoterTableManager.java
@@ -6,22 +6,30 @@ import database.exception.TableManagerException;
 
 public class PromoterTableManager {
 
+    /** 
+     * @throws TableManagerException in case of error generating executing statement
+     */
     public void createPromoterTable() throws TableManagerException {
-        String sql = "CREATE TABLE promoters ( id INTEGER IDENTITY, name VARCHAR(256), secondname VARCHAR(256), email VARCHAR(256), age INTEGER)";
+        // SQL format be like: "CREATE TABLE PROMOTERS (id INTEGER IDENTITY, name VARCHAR(256), secondname VARCHAR(256), email VARCHAR(256), age INTEGER)"
+        String sql = "CREATE TABLE PROMOTERS (id INTEGER IDENTITY, name VARCHAR(256), secondname VARCHAR(256), email VARCHAR(256), age INTEGER)";
         try {
             DBManager.generateStatementEx(sql);
         } catch (DBManagerException e) {
-            throw new TableManagerException("Error creating promoter table");
+            throw new TableManagerException("Error creating promoters table");
         }
     }
     
+    
+    /** 
+     * @throws TableManagerException in case of error generating executing statement
+     */
     public void deletePromoterTable() throws TableManagerException {
-        String sql = "DROP TABLE promoters";
+        String sql = "DROP TABLE PROMOTERS";
         try {
             DBManager.generateStatementEx(sql);
         } catch(DBManagerException e)
         {
-            throw new TableManagerException("Error deleting promoter table");
+            throw new TableManagerException("Error deleting promoters table");
         }
     }
 
