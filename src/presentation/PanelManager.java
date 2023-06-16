f9857d0a28fdd8cb0fbc13b1740e8fbc35312658
juanma <jugoka947@gmail.com>
2023-06-15 21:45:03 -0300

    panelmanager cont

--- src/presentation/PanelManager.java
+++ src/presentation/PanelManager.java
@@ -1,6 +1,12 @@
 package presentation;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 
 public class PanelManager {
     private JFrame frame;
@@ -14,6 +20,20 @@ public class PanelManager {
     public void initializeManager() {
         frame = new JFrame();
         frame.setBounds(100, 100, 500, 500);
+        frame.setLocationRelative(null);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                if(DialogManager.ShowConfirmationMessage == JOptionPane.YES_OPTION) {
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                } else {
+                    frame.setDefaultCloseOperation(JFrame.DO_NOTHING);
+                }
+            }
+        });
+        promoterRegistrationPanel = new PromoterRegistrationPanel(this);
+        promoterListPanel =  new PromoterListPanel(this);
+        new StartMainPanel(this);
+
     }
 
 }
